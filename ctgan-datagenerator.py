2879e160670aae82b1717d86edb0f2da6d5b1816
alivnavc <167910354+alivnavc@users.noreply.github.com>
2024-07-28 18:07:57 +0000

    Updated the title

--- ctgan-datagenerator.py
+++ ctgan-datagenerator.py
@@ -58,7 +58,7 @@ from sdv.single_table import CTGANSynthesizer
 from sdv.metadata import SingleTableMetadata
 
 def main():
-    st.title("CTGAN Data Synthesizer")
+    st.title("Data Synthesizer")
 
     # File upload
     uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"])
